d5f880fb3114dd37fa771b7cee13b33100d2ddb4
yvannmekenese <190848664+YvannMekenese@users.noreply.github.com>
2025-06-13 16:55:14 +0200

    clarif main

--- src/main.c
+++ src/main.c
@@ -10,97 +10,66 @@
 #include "argsparse.h"
 
 int main(int argc, char **argv) {
-  /*To use debug mode: 
-   - add --debug: freud.exe --debug -f images/input/image.jpeg
-   or 
-   - Simply run your compiled project with start button on the blue menu bar at the bottom of the VS Code window.
-   */
-
-  /*DO NOT EDIT THIS PART*/
   Config configuration ;
   parse_arguments( argc, argv, &configuration ) ;
   check_debug_mode(configuration);
   check_file();
-  /* END */
-
-  /* Use "if ( strncmp( command, <commandname>, 9 ) == 0 )" to check if your <commandname> is called by program.*/
-  /* Example with helloworld command
-   * If helloworld is a called command: freud.exe -f images/input/image.jpeg -c helloworld 
-   */
   if ( strncmp( configuration.command, "helloworld", 10 ) == 0 ) {
-    /* helloworld() function is defined in feature.h and implemented in feature.c */
     helloWorld();
   }
   else if ( strncmp( configuration.command, "dimension", 9 ) == 0 ) {
-    /* dimension() function is defined in feature.h and implemented in feature.c */
     dimension( configuration.filenames[0] );
   }
   else if ( strncmp( configuration.command, "first_pixel", 11 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     first_pixel(configuration.filenames[0]);
   }
   else if ( strncmp( configuration.command, "tenth_pixel", 11 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     tenth_pixel(configuration.filenames[0]);
   }
   else if ( strncmp( configuration.command, "second_line", 11 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     second_line(configuration.filenames[0]);
   }
   else if ( strncmp( configuration.command, "print_pixel", 11 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     int x = atoi(configuration.arguments[0]);
     int y = atoi(configuration.arguments[1]);
     print_pixel(configuration.filenames[0], x, y);
   }
   else if ( strncmp( configuration.command, "max_pixel", 9 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     max_pixel(configuration.filenames[0]);
   }
   else if ( strncmp( configuration.command, "min_pixel", 9 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     min_pixel(configuration.filenames[0]);
   }
   else if ( strncmp( configuration.command, "max_component", 13 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     char composante = configuration.arguments[0][0];
     max_component(configuration.filenames[0], composante);
   }
   else if ( strncmp( configuration.command, "min_component", 13 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     char composante = configuration.arguments[0][0];
     min_component(configuration.filenames[0], composante);
   }
   else if ( strncmp( configuration.command, "stat_report", 11 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     stat_report(configuration.filenames[0]);
   }
   else if ( strncmp( configuration.command, "color_red", 9 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     color_red(configuration.filenames[0]);
   }
   else if ( strncmp( configuration.command, "color_blue", 10 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     color_blue(configuration.filenames[0]);
   }
   else if ( strncmp( configuration.command, "color_green", 11 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     color_green(configuration.filenames[0]);
   }
   else if ( strncmp( configuration.command, "color_gray", 10 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     color_gray(configuration.filenames[0]);
   }
   else if ( strncmp( configuration.command, "color_invert", 12 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     color_invert(configuration.filenames[0]);
   }
   else if ( strncmp( configuration.command, "color_gray_luminance", 20 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     color_gray_luminance(configuration.filenames[0]);
   }
   else if ( strncmp( configuration.command, "rotate_cw", 20 ) == 0 ) {
-    /* first_pixel() function is defined in feature.h and implemented in feature.c */
     rotate_cw(configuration.filenames[0]);
   }
   return 0;
